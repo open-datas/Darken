1c754d37f5881927eee518b2bb27a5a76dcbf8f6
houphiler@sina.com <houphiler@sina.com>
2019-09-10 11:32:09 +0800

    处理清理相关工程

---
+++ disposal/src/test/java/com/info/disp/server/DispApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hone.disp.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DispApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
